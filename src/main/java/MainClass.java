ac6e8f653e1a0beb242ecbe27a1ae7ee60de1b2f
AlexandrMay <maysalexandr@gmail.com>
2018-07-20 16:44:22 +0300

    first

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,11 @@
+import com.google.common.math.DoubleMath;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        double a = DoubleMath.factorial(1);
+        System.out.println(a);
+    }
+
+}
