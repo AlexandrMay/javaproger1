76dc18ca7e32a4e6a7435afbe4207c146019aa57
AlexandrMay <maysalexandr@gmail.com>
2018-07-23 17:49:25 +0300

    task memory

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,15 +1,31 @@
-import com.google.common.math.DoubleMath;
+import java.lang.management.ManagementFactory;
 
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
 
-        double a = DoubleMath.factorial(1);
-        System.out.println(a);
+        int size = 20000000;
+
+        System.out.println("Starting the loop");
+        while (true) {
+            System.gc();
+            Thread.sleep(10);
+            Runtime runtime = Runtime.getRuntime();
+            long mem = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println(mem);
+            Object[] array = new Object[size];
+            long mem1 = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println((mem1 - mem)/size);
+            System.out.println("New array of size: " + array.length + " created");
+            for (int i = 0; i < size; i++) {
+                array[i] = new Object();
+            }
+            System.out.println("Created " + size + " objects.");
+            Thread.sleep(1000);
+        }
 
 
-        String word = "Hello, world";
-        for (char c: word.toCharArray()) System.out.println(c);
 
     }
 
